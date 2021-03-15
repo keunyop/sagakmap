bcf7c2c654ec3cd6548b2afa0a8ab3c550816838
keunyop <kylee1112@hotmail.com>
2021-03-16 05:42:47 +0900

    지도개발중

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 // 지도를 생성한다 
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
+var clusterer = new kakao.maps.MarkerClusterer({
+    map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+    averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+    minLevel: 10 // 클러스터 할 최소 지도 레벨 
+});
 
 let churchs = [
-    { x: 37.39234762613166, y: 127.13087848124754, name: '분당우리교회(송림)' },
+    { x: 37.39234762613166, y: 127.13087848124754, name: '분당우리교회(송림본당)' },
     { x: 37.385095301113125, y: 127.11948028227279, name: '분당우리교회(드림센터)' }
 ];
+
+let markers = [];
 
 churchs.forEach(church => {
     // 마커가 표시될 위치입니다 
@@ -35,5 +42,26 @@ churchs.forEach(church => {
     });
 
     // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
-    infowindow.open(map, marker);
+    // infowindow.open(map, marker);
+    markers.push(marker);
+
+    kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+    kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
 });
+
+// 클러스터러에 마커들을 추가
+clusterer.addMarkers(markers);
+
+// 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
+function makeOverListener(map, marker, infowindow) {
+    return function () {
+        infowindow.open(map, marker);
+    };
+}
+
+// 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+function makeOutListener(infowindow) {
+    return function () {
+        infowindow.close();
+    };
+}
