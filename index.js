6549bdc59d873fc1b2d1c5ebd4708908ac4b8abc
keunyop <kylee1112@hotmail.com>
2021-03-17 06:41:33 +0900

    custom overlay 추가중

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ let churchs = [
 ];
 
 let markers = [];
+var overlay;
 
 churchs.forEach(church => {
     // 마커가 표시될 위치입니다 
@@ -33,20 +34,55 @@ churchs.forEach(church => {
     // 마커가 지도 위에 표시되도록 설정합니다
     marker.setMap(map);
 
-    var iwContent = '<div style="padding:5px;">' + church.name + '</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    // var iwContent = '<div style="padding:5px;">' + church.name + '</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
 
-    // 인포윈도우를 생성합니다
-    var infowindow = new kakao.maps.InfoWindow({
-        position: markerPosition,
-        content: iwContent
+    // // 인포윈도우를 생성합니다
+    // var infowindow = new kakao.maps.InfoWindow({
+    //     position: markerPosition,
+    //     content: iwContent
+    // });
+
+    // 커스텀 오버레이에 표시할 컨텐츠 입니다
+    // 커스텀 오버레이는 아래와 같이 사용자가 자유롭게 컨텐츠를 구성하고 이벤트를 제어할 수 있기 때문에
+    // 별도의 이벤트 메소드를 제공하지 않습니다 
+    var content = '<div class="wrap">' +
+        '    <div class="info">' +
+        '        <div class="title">' + church.name +
+        '            <div class="close" onclick="closeOverlay();" title="닫기"></div>' +
+        '        </div>' +
+        '        <div class="body">' +
+        '            <div class="img">' +
+        '                <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
+        '            </div>' +
+        '            <div class="desc">' +
+        '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
+        '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+        '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+        '            </div>' +
+        '        </div>' +
+        '    </div>' +
+        '</div>';
+
+    // 마커 위에 커스텀오버레이를 표시합니다
+    // 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
+    overlay = new kakao.maps.CustomOverlay({
+        content: content,
+        map: map,
+        position: marker.getPosition()
+    });
+
+    // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
+    kakao.maps.event.addListener(marker, 'click', function () {
+        overlay.setMap(map);
     });
 
     // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
-    // infowindow.open(map, marker);
     markers.push(marker);
 
-    kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
-    kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+    // kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+    // kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+
+
 });
 
 // 클러스터러에 마커들을 추가
@@ -64,4 +100,9 @@ function makeOutListener(infowindow) {
     return function () {
         infowindow.close();
     };
+}
+
+// 커스텀 오버레이를 닫기 위해 호출되는 함수입니다 
+function closeOverlay() {
+    overlay.setMap(null);
 }
