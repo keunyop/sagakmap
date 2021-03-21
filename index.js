bd52730af805f3f5446c569af0fd31b04944bab0
keunyop <kylee1112@hotmail.com>
2021-03-22 05:30:33 +0900

    타이틀 수정

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var mapOptions = {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 10
+    center: new naver.maps.LatLng(37.39234762613166, 127.13087848124754),
+    zoom: 16
 };
 
 var map = new naver.maps.Map('map', mapOptions);
